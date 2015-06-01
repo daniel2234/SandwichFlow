fe34b1592a9d232bba53017104cd5a1c6b29c63e
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-06-01 16:02:13 -0400

    Initial Commit

---
+++ SandwichFlow/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SandwichFlow
+//
+//  Created by Daniel Kwiatkowski on 2015-06-01.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SandwichFlow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SandwichFlow
+//
+//  Created by Daniel Kwiatkowski on 2015-06-01.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
